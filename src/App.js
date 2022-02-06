82b01451f3da6a5be390a09a459ce4061ea067aa
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-05 23:59:15 -0500

    test web

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route exact path = '/' element={<LandingPage />}></Route>
         <Route exact path = 'about' element={<AboutPage />}></Route>
-        <Route path = "*" element={<ErrorPage />}></Route>
+        
       </Routes>
     </div>
   );
