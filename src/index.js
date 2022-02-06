10d933b488230000a39bc5a96b9437d7ac41e8de
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-06 00:14:07 -0500

    test web

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL + '/'}>
       <App />
     </HashRouter>
   ,
